3444ee58e4acae4bcf7510155613181326b5f6b6
Dimeji <adedimeji88@yahoo.com>
2022-10-27 13:37:19 +0100

    Api that return a json file with my details

---
+++ zuri-week-1/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace zuri_week_1.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly User User;
+        public UserController()
+        {
+            User = new User
+            {
+                UserName = "aregbesola88",
+                Age = 16,
+                Bio = " am an enthusiastic, highly optimistic agile software engineer," +
+                " passionate about technology and known to pick up new skills in no short time.",
+                Backend = true,
+            };
+        }
+
+        [HttpGet("Get-User")]
+        public IActionResult Get()
+        {
+            var user = User;
+            return Ok(user);
+        }
+    }
+}
---
+++ zuri-week-1/User.cs
@@ -0,0 +1,10 @@
+﻿namespace zuri_week_1
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+        public bool Backend { get; set; }
+    }
+}
